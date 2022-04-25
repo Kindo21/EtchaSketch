b8f50865c51ed226c658676915dfcb8759ac6171
Gregory Krieger <1111589@palmbeach.k12.fl.us>
2022-04-25 15:39:27 -0400

    Second Commit to get work into github

--- javascript.js
+++ javascript.js
@@ -1,13 +1,31 @@
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
+let rows;
+let cols;
+let target;
+
+/* set cells to an array somehow and then set attribute to all cells in array and write array to DOM.*/
+
+function div(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    //cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
-  };
-};
+    target = container.querySelector(".grid-item");
+  }
+}
 
-makeRows(16, 16);
+div(16, 16);
+function enter() {
+    target.setAttribute("style", "background-color:black;")
+}
+/*function leave() {  
+    target.setAttribute("style", "background-color:white;")
+ }*/
+
+target.addEventListener("mouseover", enter, false);
+/*target.addEventListener("mouseout", leave, false);*/
+
+
+

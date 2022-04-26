0eb5789028ba88510c3f77d068bce37851ba6f74
Gregory Krieger <1111589@palmbeach.k12.fl.us>
2022-04-26 07:47:49 -0400

    finished etch a sketch portion need to add reset button

--- javascript.js
+++ javascript.js
@@ -12,19 +12,25 @@ function div(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-    target = container.querySelector(".grid-item");
+    
   }
 }
 
 div(16, 16);
-function enter() {
+/*function enter() {
     target.setAttribute("style", "background-color:black;")
-}
+}*/
 /*function leave() {  
     target.setAttribute("style", "background-color:white;")
  }*/
+ //target = container.querySelector(".grid-item");
+document.querySelectorAll('.grid-item').forEach(target =>{
+    //.addEventListener("mouseover", enter, false);
+    target.addEventListener("mouseover", Event => {
+        target.setAttribute("style", "background-color:black;")
+    })
+})
 
-target.addEventListener("mouseover", enter, false);
 /*target.addEventListener("mouseout", leave, false);*/
 
 
